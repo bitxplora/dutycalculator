24e47c4a67b1b270aabd914216c9a623257e9c59
Dayo Olutayo <olutayo@olutayo.com>
2024-07-01 14:24:54 +0100

    Refactor index route to use fetch
    Changed index route from hardcoded code to retrieve the actual data from
    the API using fetch. This change prepares the code from production.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,11 @@
 import Route from '@ember/routing/route';
 
-export default class IndexRoute extends Route {
-  model() {
-    return { updatedDate: '2024-05-21:16.38' };
+export default class extends Route {
+  async model() {
+    // return { last: '2024-05-21:16.38' };
+    const response = await fetch('https://10.35.51.85:5000/ngn/update');
+    const last = await response.json();
+
+    return last;
   }
 }
