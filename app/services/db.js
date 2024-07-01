c26e9338386c2c2478de6e70840a7213f83574f0
Dayo Olutayo <olutayo@olutayo.com>
2024-07-01 20:25:19 +0100

    Add search method and searchResult getter to DB service
    Added search method that performs fetch on the API and save the result
    to the searchedItems. Also added a searchResult getter that made it
    available to others for use.

--- app/services/db.js
+++ app/services/db.js
@@ -3,7 +3,20 @@ import Service from '@ember/service';
 export default class DB extends Service {
   searchItem;
 
+  searchedItems;
+
+  server = 'https://10.35.51.85:5000';
+
   addSearchItem(item) {
     this.searchItem = item;
+  }
+
+  async search() {
+    const response = await fetch(`${this.server}ngn/items/${this.searchItem}`);
+    this.searchedItems = await response.json();
+  }
+
+  get searchResult() {
+    return this.searchedItems;
   }
 }
