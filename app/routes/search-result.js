130a0a9da7d38fbf6bddb6cdc13b8875d30c40e4
Dayo Olutayo <olutayo@olutayo.com>
2024-07-02 10:24:30 +0100

    Refactor search-result route to use DB service
    Refactored search-result route to use DB service to fetch and return
    data, rather than hardcoded data. This prepares the project for
    production.

--- app/routes/search-result.js
+++ app/routes/search-result.js
@@ -1,66 +1,11 @@
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
 
 export default class SearchResultRoute extends Route {
-  model() {
-    return [
-      {
-        cet_code: '7507200000',
-        description: 'Tube or pipe fittings',
-        id: '20',
-        vat: '7.5',
-        lvy: '',
-      },
-      {
-        cet_code: '6906000000',
-        description: 'Ceramic pipes, conduits, guttering and pipe fittings.',
-        id: '20',
-        vat: '7.5',
-        lvy: '',
-      },
-      {
-        cet_code: '7307990000',
-        description: 'Other Tube or pipe fittings not specified',
-        id: '20',
-        vat: '7.5',
-        lvy: '',
-      },
-      {
-        cet_code: '7307290000',
-        description:
-          'Other tube or pipe fittings of stainless steel not specified.',
-        id: '20',
-        vat: '7.5',
-        lvy: '',
-      },
-      {
-        cet_code: '7609000000',
-        description:
-          'Aluminium tube or pipe fittings (for example, couplings, elbows, sleeves).',
-        id: '20',
-        vat: '7.5',
-        lvy: '',
-      },
-      {
-        cet_code: '7507200000',
-        description: 'Tube or pipe fittings',
-        id: '20',
-        vat: '7.5',
-        lvy: '',
-      },
-      {
-        cet_code: '6906000000',
-        description: 'Ceramic pipes, conduits, guttering and pipe fittings.',
-        id: '20',
-        vat: '7.5',
-        lvy: '',
-      },
-      {
-        cet_code: '7307990000',
-        description: 'Other Tube or pipe fittings not specified',
-        id: '20',
-        vat: '7.5',
-        lvy: '',
-      },
-    ];
+  @service('db') db;
+
+  async model() {
+    const searchResult = await this.db.search();
+    return searchResult;
   }
 }
