39473e45c9226bb5de1056bab44175c3fe5abe69
Dayo Olutayo <olutayo@olutayo.com>
2024-07-02 17:51:55 +0100

    Refactor fetch operation to use country code as variable
    Refactored fetch operation to use country code as variable instead of
    hardcoded url. This make it easy for new countries to be easy
    supported.

--- app/services/db.js
+++ app/services/db.js
@@ -7,12 +7,16 @@ export default class DB extends Service {
 
   server = 'https://10.35.51.85:5000';
 
+  country = 'ngn';
+
   addSearchItem(item) {
     this.searchItem = item;
   }
 
   async search() {
-    const response = await fetch(`${this.server}/ngn/items/${this.searchItem}`);
+    const response = await fetch(`
+      ${this.server}/${this.country}/items/${this.searchItem}
+      `);
     const result = await response.json();
 
     return result;
