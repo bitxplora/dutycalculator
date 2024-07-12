261903e53930c83a0dcdf0cad8bfa2f693af265b
Dayo Olutayo <olutayo@olutayo.com>
2024-07-12 21:21:55 +0100

    Add maxWidth of 250 to tippyTip modifier.
    Added maxWidth of 250 that determines the tooltip box as default value
    for tippyTip modifier as against the 350 default set my tippy. This
    improves the visual look.

--- app/modifiers/tippyTip.js
+++ app/modifiers/tippyTip.js
@@ -7,23 +7,6 @@ import 'tippy.js/themes/material.css';
 import 'tippy.js/themes/translucent.css';
 import 'tippy.js/animations/scale.css';
 
-/*
- *
- * This modifier enables adding of tooltip to any element.
- *
- * @param {HTMLElement} element - This is the element on wish the modifier is
- *                 applied, it is the default, it is not needed to be supplied.
- *                 Default to the element the modifier is applied on.
- * @param {[Event]} trigger - The that must happened for this effect to happen. Required
- * @param {[Placement]} position - It is where the tooltip be placed. Required,
- *                  default to top, consult tippy.js for other options.
- * @param {[Tip]} message - It is tips that will be displayed. Required.
- * @param {[Props]} tippyOptions - It is object of props, for customisation of behavior. Optional.
- * @return a teardown callback, that removes the tooltip from the element.
- *
- * example {{tippyTip 'click' 'right' 'I am a tooltip!' {theme: 'customised'}}}
- *
- */
 export default modifier(
   (element, [trigger, position, message, tippyOptions]) => {
     const defaultOptions = {
@@ -31,9 +14,9 @@ export default modifier(
       trigger: trigger,
       placement: position,
       theme: 'tippyTheme',
-      animateFill: true,
       inertia: true,
       animation: 'scale',
+      maxWidth: 250,
     };
 
     const tippyRef = tippy(element, { ...defaultOptions, ...tippyOptions });
