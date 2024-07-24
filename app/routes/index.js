6091b3167bfec4de7c9c258c7f185411eff24cca
Dayo Olutayo <olutayo@olutayo.com>
2024-07-24 16:59:25 +0100

    Clean up of clutter

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,6 @@ import Route from '@ember/routing/route';
 
 export default class extends Route {
   async model() {
-    // return { last: '2024-05-21:16.38' };
     const response = await fetch('https://10.35.51.85:5000/ngn/update');
     const last = await response.json();
 
