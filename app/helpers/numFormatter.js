df8544aca1537ecfa3d312617eb3684fe6cc0b4b
Dayo Olutayo <olutayo@olutayo.dev>
2024-11-15 17:43:29 +0100

    Added documentation to the numFormatter helper

--- app/helpers/numFormatter.js
+++ app/helpers/numFormatter.js
@@ -1,3 +1,8 @@
+/**
+ * @param number - the number or figure to format into currency e.g 10000
+ * @param options - the format options
+ * @returns string e.g 10,000.00
+ */
 export default function numFormatter(
   number,
   options = {
