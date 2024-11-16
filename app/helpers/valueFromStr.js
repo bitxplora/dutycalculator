a08d35f830ca46cd029974b7e36ea08d8d708ed3
Dayo Olutayo <olutayo@olutayo.dev>
2024-11-16 13:06:11 +0100

    Added documation for valueFromStr helper

--- app/helpers/valueFromStr.js
+++ app/helpers/valueFromStr.js
@@ -1,3 +1,8 @@
+/*
+ * Removes commas seperator from number string.
+ * @param numberStr = a string of commas seperated number e.g '1,234,567'
+ * @returns string with the commas seperator removed.
+ */
 export default function valueFromStr(numberStr) {
   if (!numberStr) return 0;
   return numberStr.replace(/,/g, '');
